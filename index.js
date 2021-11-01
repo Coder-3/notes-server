96cff03384643acf092ad64ec674315f9d1c44dd
Cedric Quenette <cedricquenette@outlook.com>
2021-11-01 22:00:39 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const http = require('http')
+const notesRouter = require('./controllers/notes')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(result => console.log('connected to MongoDB'))
+  .catch(error => console.log('Error', error.message))
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/notes', notesRouter)
+
+const server = http.createServer(app)
+
+const PORT = process.env.PORT
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ models/note.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
